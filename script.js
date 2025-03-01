fef0d2659f626271f527df516fa3781bfd3ee08d
Amanda dos Santos Daros <amandsan08@gmail.com>
2025-03-01 11:59:18 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let display = document.getElementById("display");
-let currentInput=""
-let currentOperator=""
+let currentInput="";
+let currentOperator="";
 
 
 function appendNumber(value){
-    currentInput += value
-    display.textContent = currentInput
+    currentInput += value;
+    display.textContent = currentInput;
 }
 function appendOperator(operator){
     if (currentInput === "" && operator !== ".") return;
@@ -14,14 +14,14 @@ function appendOperator(operator){
 }
 function calculate(){
     try{
-       let result=eval(currentInput)
+       let result=eval(currentInput);
        if(!Number.isInteger(result)){
-        result= result.toFixed(2)
+        result= result.toFixed(2);
        }
        currentInput= result;
        display.textContent = currentInput;
     }catch(error){
-       display.textContent = "error"
+       display.textContent = "error";
        currentInput = "";
     }
 }
